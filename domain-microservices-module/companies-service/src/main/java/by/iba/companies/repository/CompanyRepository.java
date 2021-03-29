f078ec0b6fb49df593754c9cf1c0f5414ed1e0da
MariaRozhkova <mary.wootsen.96@gmail.com>
2021-03-29 18:08:24 +0300

    Use JpaRepository in CompanyRepository

--- domain-microservices-module/companies-service/src/main/java/by/iba/companies/repository/CompanyRepository.java
+++ domain-microservices-module/companies-service/src/main/java/by/iba/companies/repository/CompanyRepository.java
@@ -2,13 +2,13 @@ package by.iba.companies.repository;
 
 import by.iba.companies.domain.Company;
 import by.iba.companies.domain.CompanyId;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface CompanyRepository extends CrudRepository<Company, CompanyId> {
+public interface CompanyRepository extends JpaRepository<Company, CompanyId> {
 
     Optional<Company> findByUNP(final String unp);
 
