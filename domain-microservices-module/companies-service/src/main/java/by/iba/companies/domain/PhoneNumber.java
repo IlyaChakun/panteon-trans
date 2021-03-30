7efb387db083db85c347c9a1019f3f4ff3dae351
MariaRozhkova <mary.wootsen.96@gmail.com>
2021-03-30 17:11:37 +0300

    Change PhoneNumber class; Ude unidirectional mapping instead of bidirectional

--- domain-microservices-module/companies-service/src/main/java/by/iba/companies/domain/PhoneNumber.java
+++ domain-microservices-module/companies-service/src/main/java/by/iba/companies/domain/PhoneNumber.java
@@ -1,5 +1,6 @@
 package by.iba.companies.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,14 +12,15 @@ import javax.persistence.*;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class PhoneNumber {
 
     @Id
     @Column(name = "value", nullable = false, unique = true)
     private String value;
 
-    @ManyToOne
-    @JoinColumn(name = "company_id")
-    private Company company;
+//    @ManyToOne
+//    @JoinColumn(name = "company_id")
+//    private Company company;
 
 }
