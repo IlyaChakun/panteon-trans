d17a839af85a60d8f5a127ee39a1e0109264da51
ShteinRose <enotpoloskun08062001@gmail.com>
2021-06-07 16:01:42 +0300

    version added

--- libs-module/lib-common-payload/src/main/java/by/iba/common/dto/BaseDTO.java
+++ libs-module/lib-common-payload/src/main/java/by/iba/common/dto/BaseDTO.java
@@ -1,9 +1,22 @@
 package by.iba.common.dto;
 
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import java.io.Serializable;
 
 public abstract class BaseDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private int version;
+
+    @PrePersist
+    public void setVersion() {
+        version = 1;
+    }
+
+    @PreUpdate
+    public void updateVersion() {
+        version++;
+    }
 }
