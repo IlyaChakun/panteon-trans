f06479c9be7277798be1a4b28714a6604cab36b8
ShteinRose <enotpoloskun08062001@gmail.com>
2021-07-30 19:39:37 +0300

    auth fixed

--- domain-microservices-module/auth-service/src/main/java/by/iba/AuthApplication.java
+++ domain-microservices-module/auth-service/src/main/java/by/iba/AuthApplication.java
@@ -1,10 +1,18 @@
 package by.iba;
 
+import com.netflix.appinfo.AmazonInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
@@ -14,10 +22,29 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @SpringBootApplication(scanBasePackages = {"by.iba"})
 @EnableFeignClients(basePackages = "by.iba.client")
 @EnableCircuitBreaker
+@EnableEurekaClient
+@Slf4j
 public class AuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
+	}
+
+	@Bean
+	@Autowired
+	@Profile("prod")
+	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils inetUtils) {
+		log.info("** Inside eurekaInstanceConfigBean **");
+		EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+		AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+		config.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+		config.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+		config.setDataCenterInfo(info);
+		log.info("*** LOCAL HOSTNAME: {}", info.get(AmazonInfo.MetaDataKey.localHostname));
+		log.info("*** LOCAL IP: {} ", info.get(AmazonInfo.MetaDataKey.localIpv4));
+		log.info("*** PUBLIC HOSTNAME: {}", info.get(AmazonInfo.MetaDataKey.publicHostname));
+		log.info("*** PUBLIC IP: {}", info.get(AmazonInfo.MetaDataKey.publicIpv4));
+		return config;
 	}
 
 }
